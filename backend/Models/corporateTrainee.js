96870b56395aa5993818899e9400d29cb5eff12e
alafty <mohamedelelafty@yahoo.com>
2022-11-03 13:08:50 +0100

    created courses routes and controller

---
+++ backend/Models/corporateTrainee.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Course = require('./course')
+
+//MIGHT MERGE WITH INDIV TRAINEE AND MAKE A CORPORATE AS AN ATTRIBUTE
+const corpTraineeSchema = new Schema({
+  Username: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true
+  },
+  Password: {
+    type: String,
+    required: true,
+  },
+  PurchasedCourses: {
+    type: [Course],
+    required: true
+  },
+  Corporate:{
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+const corpTrainee = mongoose.model('CorpTrainee', corpTraineeSchema);
+module.exports = corpTrainee;
