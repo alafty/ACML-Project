92deffdf5dd6fab4efeae65d062e4034ec10716f
KhaledNowara <kn_nowara@outlook.com>
2022-11-06 23:39:01 +0100

    Filter by instructor

--- Backend/Controllers/filterController.ts
+++ Backend/Controllers/filterController.ts
@@ -13,6 +13,21 @@ const getBySubject= async (req: Request, res: Response) => {
       }
     // filter specific subjects
 }
+const getBySubjectInstructor= async (req: Request, res: Response) => {
+  //used Acml as test name, replace to get the required name from the user
+  // Courses should have attribute subject in them 
+  var subjet = req.body.Subject
+  var instructor = req.body.Instructor
+
+  const courses = await coursesModel.find({Subject: subjet},{Instructor:instructor});
+  try {
+      res.send(courses);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  // filter specific subjects
+}
+
 
 const getByRating = async (req: Request, res: Response) => {
     //used 3 as test rating, replace to get the required name from the user
@@ -41,5 +56,21 @@ const getByPrice = async (req: Request, res: Response) => {
     // filter in price range
 }
 
+
+const getByPriceInstructor = async (req: Request, res: Response) => {
+  //used 0,1000 as test prices, replace to get the required name from the user
+  var factorSmaller  = req.body.PriceLow;
+  var factorGreater = req.body.PriceHigh;
+  var instructor = req.body.Instructor
+  const courses = await coursesModel.find({
+   Price: { $gt: factorGreater,$lt: factorSmaller }},{Instructor:instructor});
+  try {
+      res.send(courses);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+   // filter in price range
+}
+
 export {
-    getBySubject, getByRating, getByPrice};
+    getBySubject,getBySubjectInstructor, getByRating, getByPrice,getByPriceInstructor};
--- Backend/Routes/filterRoutes.ts
+++ Backend/Routes/filterRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 const FiltersRouter = Router();
-import { getBySubject, getByRating, getByPrice } from "../Controllers/filterController";
+import { getBySubject,getBySubjectInstructor, getByRating, getByPrice, getByPriceInstructor } from "../Controllers/filterController";
 
 FiltersRouter.get("/subject", getBySubject);
 FiltersRouter.get("/rating", getByRating);
 FiltersRouter.get("/price", getByPrice);
+FiltersRouter.get("/subjectInstructor", getBySubjectInstructor);
+FiltersRouter.get("/priceInstructor", getByPriceInstructor);
 
 
 export default FiltersRouter;
