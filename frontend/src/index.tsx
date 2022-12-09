49a29e2bbb77c02ad5ac521f4740e3a178565ea0
alafty <mohamedelelafty@yahoo.com>
2022-12-09 10:38:30 +0100

    fixed more import errors

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.tsx';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
