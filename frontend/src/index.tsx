e746239946656bcc421a0a0b57a8a9531c426496
alafty <mohamedelelafty@yahoo.com>
2022-12-09 10:32:05 +0100

    Fixed wrong paths and converted js files to ts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
