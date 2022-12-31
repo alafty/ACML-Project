c1bd5cdb5078377994e49f558b849242c678550b
TofyLion <tofy2001@hotmail.com>
2022-12-31 20:21:58 +0100

    Added token saving to register and login - Created a function to add token to axios in order to send in header of requests

---
+++ frontend/src/utils/authUtils.ts
@@ -0,0 +1,19 @@
+const setToken = (token: string) => {
+    localStorage.setItem("token", token);
+}
+
+/// To use token, use as follows:
+/// ```
+/// config = { 
+///     headers: { 
+///         ...getTokenHeader() 
+///     } 
+/// }
+/// ```
+/// Then use the config in axios normally
+const getTokenHeader = () => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    return { Authorization: `Bearer ${token}` }
+}
+
+export {setToken, getTokenHeader}
