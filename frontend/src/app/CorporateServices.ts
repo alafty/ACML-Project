84c758d3d8080f1f1344098b156b9c7472163283
Mohamed El-Alafty <96385365+alafty@users.noreply.github.com>
2023-01-14 23:06:08 +0100

    added trainee addition by a corporate

--- frontend/src/app/CorporateServices.ts
+++ frontend/src/app/CorporateServices.ts
@@ -46,11 +46,44 @@ export const fetchCorporates = async () => {
   });
   }
 
+  export const addTrainee = async (
+    username: String,
+    password: String,
+    email: String,
+    corporate: String,
+    callback: any
+  ) => {
+    var data = qs.stringify({
+      Username: username,
+      Password: password,
+      Email: email,
+      Corporate: corporate
+    })
+    var config = {
+    method: 'post',
+    url: 'http://localhost:8000/create/corporateTrainee',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: data
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+    callback(response.data);
+  })
+  .catch(function (error) {
+    callback(error.response.data.message, true);
+  });
+  }
+
 
 
 const Services = {
     fetchCorporates,
-    changePackage
+    changePackage,
+    addTrainee
 }
 
 export default Services;
