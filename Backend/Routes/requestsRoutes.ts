acdaa2fbd0803e86962dc711a9f625238c1777d9
fr-gr <96599199+fr-gr@users.noreply.github.com>
2023-01-17 19:20:40 +0200

    accept requests done

--- Backend/Routes/requestsRoutes.ts
+++ Backend/Routes/requestsRoutes.ts
@@ -1,10 +1,11 @@
 
 import { Router } from "express";
-import { createRequest, fetchRequests } from "../Controllers/requestsController";
+import { createRequest, fetchRequests,acceptRequests } from "../Controllers/requestsController";
 
 const requestRouter = Router();
 
 requestRouter.post('/create', createRequest);
 requestRouter.post('/fetch', fetchRequests);
+requestRouter.delete('/accept',acceptRequests);
 
 export default requestRouter;
